1a07fc078d6940432680c88d0fce19a217e76555
Steven Orc <stevencloudorc@gmail.com>
2024-09-16 23:19:14 +0200

    :wrench: feat(core): posgresQL db connection to the project

--- src/config/database.ts
+++ src/config/database.ts
@@ -8,11 +8,11 @@ export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-  username: process.env.DB_USER || 'your_username',
-  password: process.env.DB_PASSWORD || 'your_password',
-  database: process.env.DB_NAME || 'productdb',
-  entities: [Product],
-  synchronize: true, // Utilisé pour synchroniser la base de données avec les entités. Désactive en production
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [Product], // Your entities here
+  synchronize: true, // Set to false in production
   logging: true,
 });
 
